3b4d98f1597667a31a9ac5d38b0e39ab8db07afe
ghpark <pkh879@gmail.com>
2021-11-30 22:22:12 +0900

    [ADD] create new project

---
+++ src/main/java/com/ghpark/kafkamsademo/KafkaMsaDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ghpark.kafkamsademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaMsaDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaMsaDemoApplication.class, args);
+    }
+
+}
